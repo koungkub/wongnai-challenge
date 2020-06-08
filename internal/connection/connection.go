1afed17bd32c783eef2519e4a7e1af9e823c41bd
koungkub <1997jirasak@gmail.com>
2020-06-08 15:11:50 +0700

    add func create logging instance

--- internal/connection/connection.go
+++ internal/connection/connection.go
@@ -4,7 +4,23 @@ import (
 	"database/sql"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
+
+// NewLog create loggin instance or connection to centralized logging server
+func NewLog(service string) {
+
+	log := logrus.New()
+
+	log.SetLevel(logrus.DebugLevel)
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	l := log.WithFields(logrus.Fields{
+		"service-name": service,
+	})
+
+	return l
+}
 
 // NewDB create database sql connection to remote server
 func NewDB(driver, dsn string) (*sql.DB, error) {
