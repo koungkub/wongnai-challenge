95d2313fad0f2bce16d6e3aaa2b0c84eab0a087e
koungkub <1997jirasak@gmail.com>
2020-06-09 14:01:28 +0700

    add handler for rouing

---
+++ internal/handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import "github.com/gofiber/fiber"
+
+// GetReviewByID handler get review by specific review_id
+func GetReviewByID() func(c *fiber.Ctx) {
+	return func(c *fiber.Ctx) {
+
+	}
+}
+
+// SearchReviewByQuery handler get review by specific review_keyword
+func SearchReviewByQuery() func(c *fiber.Ctx) {
+	return func(c *fiber.Ctx) {
+
+	}
+}
+
+// EditReview handler edit review by specific review_id
+func EditReview() func(c *fiber.Ctx) {
+	return func(c *fiber.Ctx) {
+
+	}
+}
