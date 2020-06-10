943e3baa62b078709076cf05d574441a25cd8d3a
koungkub <1997jirasak@gmail.com>
2020-06-10 14:31:20 +0700

    add env variable and get connection instance

--- cmd/wongnai/main.go
+++ cmd/wongnai/main.go
@@ -1,10 +1,39 @@
 package main
 
-import "github.com/koungkub/wongnai/internal/route"
+import (
+	"strings"
+
+	"github.com/koungkub/wongnai/internal/connection"
+	"github.com/koungkub/wongnai/internal/model"
+	"github.com/koungkub/wongnai/internal/route"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("env")
+	viper.AddConfigPath("./config")
+	viper.ReadInConfig()
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+}
 
 func main() {
 
-	r := route.New()
+	svcName := viper.GetString("WONGNAI.NAME")
+	log := connection.NewLog(svcName)
+
+	host, pass := viper.GetString("APP.CACHE.HOST"), viper.GetString("APP.CACHE.PASS")
+	cache := connection.NewCache(host, pass)
+
+	driver, dsn := viper.GetString("APP.DB.DRIVER"), viper.GetString("APP.DB.DSN")
+	db, err := connection.NewDB(driver, dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	conf := model.NewConf(db, log, cache)
+
+	r := route.New(conf)
 
 	r.Listen(3000)
 }
