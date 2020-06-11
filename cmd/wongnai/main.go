afbd02974f6014a1292bf7739114fc0ce413b3c6
koungkub <1997jirasak@gmail.com>
2020-06-11 14:34:56 +0700

    add mysql driver

--- cmd/wongnai/main.go
+++ cmd/wongnai/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/koungkub/wongnai/internal/model"
 	"github.com/koungkub/wongnai/internal/route"
 	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
