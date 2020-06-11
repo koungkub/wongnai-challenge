c464f0fd65f349696d1cdf186fb16e33b3594f8f
koungkub <1997jirasak@gmail.com>
2020-06-11 14:34:37 +0700

    add business logic for func getreviewbyid

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,11 +1,35 @@
 package handler
 
-import "github.com/gofiber/fiber"
+import (
+	"time"
+
+	"github.com/gofiber/fiber"
+	"github.com/koungkub/wongnai/internal/model"
+	"github.com/koungkub/wongnai/internal/worker"
+)
 
 // GetReviewByID handler get review by specific review_id
 func GetReviewByID() func(c *fiber.Ctx) {
 	return func(c *fiber.Ctx) {
+		id := c.Params("id")
+		conf := c.Locals("conf").(*model.Conf)
+		r := worker.NewReview(conf.DB, conf.Cache)
 
+		review, err := r.GetReviewByCache(id)
+		if err != nil {
+			review, err = r.GetReviewByDB(id)
+			if err != nil || len(review) == 0 {
+				c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+					"message": "id not match any reviews",
+				})
+				return
+			}
+			r.SetReviewInCache(id, review, time.Hour)
+		}
+
+		c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"data": review,
+		})
 	}
 }
 
