c5a59b9b3e9460c042203ff8f99cf1f22136dc49
koungkub <1997jirasak@gmail.com>
2020-06-12 15:54:45 +0700

    arrange interface method readable

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -6,10 +6,14 @@ type Reviewer interface {
 	GetReviewInCache(id string) (string, error)
 	GetReviewInDB(id string) (string, error)
 	SetReviewInCache(id string, review string, exp time.Duration) error
-	SearchReviewByKeywordInDB(keyword string) ([]string, error)
-	SearchReviewByKeywordInCache(keyword string) ([]string, error)
-	SetReviewKeywordInCache(keyword string, reviews []string) error
+
 	SearchKeywordInCache(keyword string) (string, error)
 	SearchKeywordInDB(keyword string) error
 	SetKeywordInCache(keyword string, review string, exp time.Duration) error
+
+	SearchReviewByKeywordInDB(keyword string) ([]string, error)
+	SearchReviewByKeywordInCache(keyword string) ([]string, error)
+	SetReviewKeywordInCache(keyword string, reviews []string) error
+
+	EditReviewInDB(id, review string) (int64, error)
 }
